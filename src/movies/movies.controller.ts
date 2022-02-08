6625b0ef0a5a067045e88d4bb76e88f68a869b97
ErranderLee <dre12am14@gmail.com>
2022-02-08 18:18:14 +0900

    controller structure

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,39 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+
+@Controller('movies')
+export class MoviesController {
+    @Get()
+    getAll(){
+        return "This will return all movies";
+    }
+
+    @Get('search')
+    search(@Query("year") searchingYear: string) {
+        return `We are searching for a movie made after ${searchingYear}`;
+    }
+    
+
+    @Get(":id")
+    getOne(@Param("id") id: string){
+        return `This will return one movie with the id: ${id}`;
+    }
+
+    @Post()
+    create(@Body() movieData){
+        console.log(movieData);
+        return movieData.name;
+    }
+
+    @Delete(":id")
+    remove(@Param('id') id: string) {
+        return `This will delete a movie ${id}`;
+    }
+
+    @Patch(':id')
+    path(@Param('id') id: string, @Body() updateData){
+        return {
+            updateMovie : id,
+            ...updateData
+        }
+    }
+}
